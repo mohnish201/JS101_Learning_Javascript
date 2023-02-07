a33f58ee80e2dc7d864df53e5900d085f33662ad
Bhavna Vishwakarma <bhawnavish99@gmail.com>
2023-02-07 11:34:20 +0530

    variables

---
+++ Problem2.js
@@ -0,0 +1,8 @@
+let name = "Mohnish Vishwakarma";
+console.log("Name: ",name);
+
+name = "Dhanraj Vishwakarma";
+console.log("Father's name: ", name);
+
+name = "Pushpa Vishwakarma";
+console.log("Mother's name: ", name);
---
+++ Problem3.js
@@ -0,0 +1,5 @@
+let name = "Mohnish Vishwakarma";
+let age = 21;
+console.log(name,age);
+
+console.log(typeof(name),typeof(age));
